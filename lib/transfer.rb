bfef0fc6efefbbd8de674aa1f544cd8c0ca8f34e
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 18:43:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = reciever
+    @amount = amount
+  end
 end
