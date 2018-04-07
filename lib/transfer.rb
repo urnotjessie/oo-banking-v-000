4a28d69a25046c5cad461ee401a1c36f7bc74457
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 19:00:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
 
   def initialize(sender, receiver, amount)
     @sender = sender
-    @receiver = reciever
+    @receiver = receiver
     @amount = amount
     @status = "pending"
   end
