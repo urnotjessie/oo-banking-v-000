31261eda0645bf401dddcf9580ebf2780c241f22
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 19:08:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,11 +18,11 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && self.status == "pending"
+    if self.valid? && self.status == "pending" && self.sender.balance >= self.amount 
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
-    else
+    els
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
