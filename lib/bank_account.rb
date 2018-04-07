b564d7b8aea794d746215f7d2d8289aeb13880a4
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 17:47:53 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
+
   end
 
 
