91fa363fbdd7f9a7d2286aa394e8ccf9b59ae5f6
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 19:03:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,8 @@ class Transfer
       self.receiver.balance += self.amount
       self.status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
       self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
